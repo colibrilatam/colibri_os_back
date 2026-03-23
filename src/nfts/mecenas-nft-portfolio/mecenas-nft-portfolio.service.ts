93212ecca882b4a7fe239481475290a014d0f54d
BaptistaJose <baptistaj133@gmail.com>
2026-03-23 13:11:07 -0300

    creacion de los endpoint del mecenas nft portfolio

---
+++ src/nfts/mecenas-nft-portfolio/mecenas-nft-portfolio.service.ts
@@ -0,0 +1,38 @@
+import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { MecenasNftPortfolioRepository } from "./mecenas-nft-portfolio.repository";
+import { ICreateMecenasNft } from "./interfaces/create-mecenas-nft.interface";
+import { IUpdateMecenasNft } from "./interfaces/update-mecenas-nft.interface";
+import { UsersService } from "src/users/users.service";
+
+@Injectable()
+export class MecenasNftPortfolioService {
+    constructor(
+        private readonly mecenasNftRepository: MecenasNftPortfolioRepository,
+        private readonly userService: UsersService
+    ) { }
+
+    async createMecenasNft(data: ICreateMecenasNft) {
+        const user = await this.userService.findOneById(data.mecenasUserId);
+        const portfolio = await this.mecenasNftRepository.findByNftProjectId(data.nftProjectId);
+        if (portfolio && !portfolio.releasedAt)throw new ConflictException('El NFT ya está en el portafolio de un mecenas')
+        return await this.mecenasNftRepository.createMecenasNft({ ...data, mecenasUserId: user.id })
+    }
+
+    async findByMecenasId(mecenasUserId: string) {
+        const portfolio = await this.mecenasNftRepository.findByMecenasId(mecenasUserId);
+        if (portfolio.length === 0) throw new NotFoundException('No se encontraron NFTs en el portafolio de este mecenas')
+        return portfolio;
+    }
+
+    async findByNftProjectId(nftProjectId: string) {
+        const project = await this.mecenasNftRepository.findByNftProjectId(nftProjectId);
+        if (!project) throw new NotFoundException('No se encontró un portafolio para este proyecto de NFT')
+        return project;
+    }
+
+    async updateMecenasNft(id: string, data: IUpdateMecenasNft) {
+        const portfolio = await this.mecenasNftRepository.findById(id);
+        if (!portfolio) throw new NotFoundException('No se encontró el portafolio de mecenas NFT');
+        await this.mecenasNftRepository.updateMecenasNft(portfolio.id, data);
+    }
+}
