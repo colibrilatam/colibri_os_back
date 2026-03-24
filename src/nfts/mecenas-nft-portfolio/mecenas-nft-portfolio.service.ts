07da667942e779bfbe88f94fdabd880361e97ad9
BaptistaJose <baptistaj133@gmail.com>
2026-03-24 11:03:56 -0300

    correccion en el manejo de errores del mecenas nft

--- src/nfts/mecenas-nft-portfolio/mecenas-nft-portfolio.service.ts
+++ src/nfts/mecenas-nft-portfolio/mecenas-nft-portfolio.service.ts
@@ -3,19 +3,22 @@ import { MecenasNftPortfolioRepository } from "./mecenas-nft-portfolio.repositor
 import { ICreateMecenasNft } from "./interfaces/create-mecenas-nft.interface";
 import { IUpdateMecenasNft } from "./interfaces/update-mecenas-nft.interface";
 import { UsersService } from "src/users/users.service";
+import { NftProjectService } from "../nft-project/nfts-project.service";
 
 @Injectable()
 export class MecenasNftPortfolioService {
     constructor(
         private readonly mecenasNftRepository: MecenasNftPortfolioRepository,
-        private readonly userService: UsersService
+        private readonly userService: UsersService,
+        private readonly nftProjectService: NftProjectService
     ) { }
 
     async createMecenasNft(data: ICreateMecenasNft) {
         const user = await this.userService.findOneById(data.mecenasUserId);
         const portfolio = await this.mecenasNftRepository.findByNftProjectId(data.nftProjectId);
+        const nftProject = await this.nftProjectService.findOne(data.nftProjectId);
         if (portfolio && !portfolio.releasedAt)throw new ConflictException('El NFT ya está en el portafolio de un mecenas')
-        return await this.mecenasNftRepository.createMecenasNft({ ...data, mecenasUserId: user.id })
+        return await this.mecenasNftRepository.createMecenasNft({ ...data, mecenasUserId: user.id, nftProjectId: nftProject.id });
     }
 
     async findByMecenasId(mecenasUserId: string) {
