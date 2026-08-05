2b33d579c45b0f601ba197463427f7dd8cf4e405
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-08-05 18:43:44 -0300

    agregar importación de Roles en evaluation controller

--- src/evaluation/evaluation.controller.ts
+++ src/evaluation/evaluation.controller.ts
@@ -27,7 +27,7 @@ import { CreateRubricDto } from './dto/create-rubric.dto';
 import { UpdateRubricDto } from './dto/update-rubric.dto';
 import { JwtAuthGuard } from '../auth/guards/auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
-// import { Roles } from '../auth/decorators/roles.decorator';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { UserRole } from '../users/entities/user.entity';
 
